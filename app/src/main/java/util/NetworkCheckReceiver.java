d195fac68897be322162c655657676d2d4c47882
kimnamhyong <kimnamhyong@gmail.com>
2023-09-08 14:36:40 +0900

    first commit

---
+++ app/src/main/java/util/NetworkCheckReceiver.java
@@ -0,0 +1,35 @@
+package util;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+import co.kr.itforone.holoholic.MainActivity;
+
+public class NetworkCheckReceiver extends BroadcastReceiver {
+    public static boolean isNetwork = true;
+    ConnectivityManager connectivityManager;
+    public NetworkCheckReceiver(ConnectivityManager connectivityManager){
+        this.connectivityManager=connectivityManager;
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)){
+            if(connectivityManager.getActiveNetworkInfo()!=null){
+                if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI) != null){
+                    if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected()){
+                        isNetwork=true;
+                    }
+                }
+                if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE) != null){
+                    if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected()){
+                        isNetwork=true;
+                    }
+                }
+            }else{
+                isNetwork=false;
+            }
+        }
+    }
+}
